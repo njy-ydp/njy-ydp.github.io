be44bf76129c400ac4b6b56af320d6d6f5c69db5
njy-ydp <hakk01256@gmail.com>
2022-01-27 01:21:04 +0900

    반응형 페이지 메뉴 수정, 링크 수정

--- script.js
+++ script.js
@@ -1,7 +1,37 @@
-const menuBtn = document.querySelector("#nav-icon");
-const menu = document.querySelector("#nav-menu");
+const navBtn = document.querySelector("#nav-icon");
+const nav = document.querySelector("#nav-menu-parent");
+const navBg = document.querySelector(".menu-bg");
+const droneBtn = document.querySelector(".drone-btn");
+const droneMenu = document.querySelector("#drone-sub-parent");
 
-menuBtn.addEventListener("click", () => {
-    menu.classList.toggle("show");
-    menuBtn.classList.toggle("md-inactive");
+var isNavOpen = false;
+
+function toggleNav() {
+    if (!isNavOpen) {
+        nav.classList.add("show-nav-index");
+        nav.classList.add("show-nav");
+        navBtn.classList.remove("md-inactive");
+        isNavOpen = true;
+    }
+    else {
+        droneMenu.classList.remove("show-sub");
+        nav.classList.remove("show-nav");
+        navBtn.classList.add("md-inactive");
+        setTimeout(() => {
+            nav.classList.remove("show-nav-index");
+            isNavOpen = false;
+        }, 400)
+    }
+}
+
+navBtn.addEventListener("click", () => {
+    toggleNav();
+});
+
+navBg.addEventListener("click", () => {
+    toggleNav();
+});
+
+droneBtn.addEventListener("click", () => {
+    droneMenu.classList.toggle("show-sub");
 });
